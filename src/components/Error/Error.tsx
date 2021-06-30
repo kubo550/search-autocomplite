441e521f198886c92cadf03c35783d7af030ff90
powerty <qwercy142@gmail.com>
2021-06-30 15:09:44 +0200

    Update Comp

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,19 +1,22 @@
-import { FC, memo } from "react";
+import { FC } from "react";
+import styles from "./Error.module.css";
+import errorImg from "../../assets/error.svg";
 
 interface ErrorProps {
-  error: any;
+  error: string;
 }
 
 const refresh = () => {
   document.location.reload();
 };
 
-const Error: FC<ErrorProps> = memo(({ error }) => (
-  <div>
-    <h3> Sorry We have an error. Please try again later.</h3>
-    <p> {error.message} </p>
+const Error: FC<ErrorProps> = ({ error }) => (
+  <div className={styles.container}>
+    <h3> Sorry, We have an error. Please try again later.</h3>
+    <p> {error} </p>
     <button onClick={refresh}> Refresh </button>
+    <img src={errorImg} className={styles.img} alt='Error' />
   </div>
-));
+);
 
 export default Error;
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,5 +1,12 @@
 import styles from "./Loading.module.css";
 
-const Loading = () => <div className={styles.container}>Loading...</div>;
+const Loading = () => (
+  <div className={styles.container}>
+    <div className={styles.loader}>
+      <span></span>
+    </div>
+    <p className={styles.loading_text}>Loading...</p>
+  </div>
+);
 
 export default Loading;
