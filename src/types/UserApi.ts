9f8098eccdff64ae441e4e25a5081e0ab9d8cab5
powerty <qwercy142@gmail.com>
2021-07-01 00:25:23 +0200

    Add Types

---
+++ src/types/UserApi.ts
@@ -0,0 +1,35 @@
+interface Geo {
+    lat: number,
+    lng: number
+}
+
+interface Adress {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: Geo
+}
+
+interface Company {
+    name: string,
+    catchPhrase: string,
+    bs: string
+}
+
+export interface UserApi {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: Adress,
+    phone: string,
+    website: string,
+    company: Company
+}
+
+export interface User {
+    id: number,
+    name: string,
+    username: string,
+}
