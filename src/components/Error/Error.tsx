46fcd0041ecd04844a6c717de6dee62dfc4db4cb
powerty <qwercy142@gmail.com>
2021-07-01 01:33:16 +0200

    Update Styles

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -12,10 +12,10 @@ const refresh = () => {
 
 const Error: FC<ErrorProps> = ({ error }) => (
   <div className={styles.container}>
+    <img src={errorImg} className={styles.img} alt='Error' />
     <h3> Sorry, We have an error. Please try again later.</h3>
     <p> {error} </p>
     <button onClick={refresh}> Refresh </button>
-    <img src={errorImg} className={styles.img} alt='Error' />
   </div>
 );
 
