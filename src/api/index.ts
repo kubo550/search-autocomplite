c54ca0fbb0671a83f1456c2aee2b00ba47fc2cfe
powerty <qwercy142@gmail.com>
2021-07-01 01:33:46 +0200

    Add fetchUsers fn

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,24 @@
 import axios from "axios";
+import { Dispatch, bindActionCreators } from "redux";
+import { actionCreators, Action } from "state";
+import { UserApi } from "types/UserApi";
 
-export const fetchUsers = async () => {
-  const endpoint = "https://jsonplaceholder.typicode.com/users";
-  try {
-    const { data } = await axios.get(endpoint);
-    return data;
-  } catch (err) {
-    throw Error(err);
-  }
+const endpoint = "1https://jsonplaceholder.typicode.com/users";
+
+export const fetchUsers = (dispatch: Dispatch<Action>) => {
+
+  const { fetchUsersFailure, fetchUsersRequest, fetchUsersSuccess } =
+    bindActionCreators(actionCreators, dispatch);
+
+  fetchUsersRequest();
+
+  return (async () => {
+    try {
+      const response = await axios.get(endpoint);
+      const users = response.data as UserApi[];
+      fetchUsersSuccess(users);
+    } catch (error) {
+      fetchUsersFailure(error.message);
+    }
+  })();
 };
